9826c32ed6e1a31d45a9255bb3cdf1a9c7c0bab4
shamsher31 <ansarisam47@gmail.com>
2016-08-24 14:24:46 +0530

    Add more qoutes and customize card color

--- app/pages/home/mock-qoutes.ts
+++ app/pages/home/mock-qoutes.ts
@@ -8,22 +8,67 @@ export var QOUTES: Qoute[] = [
   },
   {
     id: '2',
-    title: 'Work hard dream big.',
-    author: 'Steve Jobs'
+    title: 'If my mind can conceive it and my heart can believe it, then I can achieve it.',
+    author: 'Muhammad Ali'
   },
   {
     id: '3',
-    title: 'Hardwork beats talent.',
-    author: 'Steve Jobs'
+    title: 'It is never too late to be what you might have been.',
+    author: 'George Eliot'
   },
   {
     id: '4',
-    title: 'Life id too short to wait.',
-    author: 'Steve Jobs'
+    title: 'If you’re going through hell keep going.',
+    author: 'Winston Churchill'
   },
   {
     id: '5',
-    title: 'Never stop dreaming.',
-    author: 'Steve Jobs'
+    title: 'Take the first step in faith, you don’t have to see the whole staircase, just take the first step.',
+    author: 'Martin Luther King, Jr'
   },
+  {
+    id: '6',
+    title: 'Success seems to be largely a matter of hanging on after others have let go.',
+    author: 'William Feather'
+  },
+  {
+    id: '7',
+    title: 'If you’re only willing to do what’s easy, life will be hard. But if you’re willing to do what’s hard, life will be easy.',
+    author: 'T. Harv Eker'
+  },
+  {
+    id: '8',
+    title: 'Do the thing you fear most and the death of fear is certain.',
+    author: 'Mark Twain'
+  },
+  {
+    id: '9',
+    title: 'When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.',
+    author: 'Alexander Graham Bell'
+  },
+  {
+    id: '10',
+    title: 'Only when we are brave enough to explore the darkness will we discover the infinite power of our light',
+    author: 'Brene Brown'
+  },
+  {
+    id: '11',
+    title: 'You miss 100% of the shots you don’t take.',
+    author: 'Wayne Gretzky'
+  },
+  {
+    id: '12',
+    title: 'Success is never final. Failure is never fatal. It is courage that counts.',
+    author: 'Winston Churchill'
+  },
+  {
+    id: '13',
+    title: 'Opportunity does not knock, it presents itself when you beat down the door.',
+    author: 'Kyle Chandler'
+  },
+  {
+    id: '14',
+    title: 'Only those who dare to fail greatly can ever achieve greatly.',
+    author: 'Robert Kennedy'
+  }
 ];
