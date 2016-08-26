75a7f8a23b77f923157858d519fe1c2502cbf05a
shamsher31 <ansarisam47@gmail.com>
2016-08-26 12:37:48 +0530

    Add more qoutes

--- app/pages/home/mock-qoutes.ts
+++ app/pages/home/mock-qoutes.ts
@@ -70,5 +70,100 @@ export var QOUTES: Qoute[] = [
     id: '14',
     title: 'Only those who dare to fail greatly can ever achieve greatly.',
     author: 'Robert Kennedy'
+  },
+  {
+    id: '15',
+    title: 'Talk to someone about themselves and they’ll listen for hours.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '16',
+    title: 'Don’t be afraid of enemies who attack you. Be afraid of the friends who flatter you.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '17',
+    title: 'I have come to the conclusion that there is only one way under high heaven to get the best of an argument – and that is to avoid it. Avoid it as you would avoid rattlesnakes and earthquakes.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '18',
+    title: 'Action seems to follow feeling, but really action and feeling go together; and by regulating the action, which is under the more direct control of the will, we can indirectly regulate the feeling, which is not.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '19',
+    title: 'Only knowledge that is used sticks in your mind.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '20',
+    title: 'It isn’t what you have or who you are or where you are or what you are doing that makes you happy or unhappy. It is what you think about it.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '21',
+    title: 'If you believe in what you are doing, then let nothing hold you up in your work. Much of the best work of the world has been done against seeming impossibilities. The thing is to get the work done.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '22',
+    title: 'You can make more friends in two months by becoming interested in other people than you can in two years by trying to get other people interested in you.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '23',
+    title: 'Big shots are only little shots who kept on shooting.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '24',
+    title: 'Winning friends begins with friendliness.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '25',
+    title: 'If some people are so hungry for a feeling of importance that they actually go insane to get it, imagine what miracle you and I can achieve by giving people honest appreciation this side of insanity.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '26',
+    title: 'People rarely succeed unless they have fun in what they are doing.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '27',
+    title: 'Don’t be afraid to give your best to what seemingly are small jobs. Every time you conquer one it makes you that much stronger. If you do the little jobs well, the big ones tend to take care of themselves.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '28',
+    title: 'Do the hard jobs first. The easy jobs will take care of themselves.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '29',
+    title: 'Why talk about what we want? That is childish. Absurd. Of course you are interested in what you want. You are eternally interested in it. But no one else is. The rest of us are just like you: we are interested in what we want.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '30',
+    title: 'By fighting, you never get enough – but by yielding, you get more than you expected.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '31',
+    title: 'It is the way we react to circumstances that determines our feelings.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '32',
+    title: 'If you can’t sleep, then get up and do something instead of lying there and worrying. It’s the worry that gets you, not the loss of sleep.',
+    author: 'Dale Carnegie'
+  },
+  {
+    id: '33',
+    title: 'Today is life-the only life you are sure of. Make the most of today. Get interested in something. Shake yourself awake. Develop a hobby. Let the winds of enthusiasm sweep through you. Live today with gusto.',
+    author: 'Dale Carnegie'
   }
 ];
