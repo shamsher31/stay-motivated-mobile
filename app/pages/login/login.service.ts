8f64fca04a21bc13a2c1de0e21fdf0785197ffc7
shamsher31 <ansarisam47@gmail.com>
2016-09-20 18:40:26 +0530

    Add Ionic Native for Facebook and Twitter login

---
+++ app/pages/login/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { TwitterConnect, Facebook } from 'ionic-native';
+
+@Injectable()
+export class LoginService {
+
+  public Twitter() {
+    TwitterConnect.login().then(this.onSuccess, this.onError);
+  }
+
+  public Facebook() {
+    Facebook.login(['public_profile']).then(this.onSuccess, this.onError);
+  }
+
+  private onSuccess(response) {
+    alert(JSON.stringify(response));
+  }
+
+  private onError(err) {
+    alert(JSON.stringify(err));
+  }
+}
--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { LoginService } from './login.service';
 
 @Component({
-  templateUrl: 'build/pages/login/login.html'
+  templateUrl: 'build/pages/login/login.html',
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController, private loginService: LoginService) { }
 
   ngOnInit() { }
+
+  loginWithTwitter() {
+    this.loginService.Twitter();
+  }
+
+  loginWithFacebook() {
+    this.loginService.Facebook();
+  }
+
 }
