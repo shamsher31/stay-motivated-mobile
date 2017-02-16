de0656450d68d97f25b27684e1488f814566eed1
shamsher31 <ansarisam47@gmail.com>
2017-02-16 18:18:51 +0530

    Display mock qoutes with delay

--- src/pages/qoute/qoute.service.ts
+++ src/pages/qoute/qoute.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import { Qoute} from './qoute';
+import { QOUTES } from './mock-qoutes';
 
 @Injectable()
 export class QouteService {
   constructor(public http: Http ) {}
 
   getQoutes(): Observable<Qoute[]> {
-    return this.http.get('/qoutes/')
-      .map((res:Response) => res.json())
+    return Observable.of(QOUTES)
+      .delay(2000)
       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
   }
 }
